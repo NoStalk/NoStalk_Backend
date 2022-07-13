42caebf1e91abb236d6480e64ca8372855759fa4
zireal23 <skasif232000@gmail.com>
2022-07-13 21:20:44 +0530

    added mongoose

--- index.ts
+++ index.ts
@@ -1,9 +1,23 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-
+const mongoString = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@nostalk.yhizah9.mongodb.net/UserDB?retryWrites=true&w=majority`;
 const app = express();
+
+mongoose.connect(mongoString);
+mongoose.connection.on("error", function (error) {
+  if (process.env.NODE_ENV === "development") {
+    console.log(error);
+  }
+});
+
+mongoose.connection.on("open", function () {
+  console.log("Connected to MongoDB database.");
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
